a39c5e68614be8df6423e7d58cfecdf9fa8b51d0
emre1n <emre1n@protonmail.com>
2023-02-23 13:28:18 +0300

    Add error handling and refactor

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,50 @@ import { TMovie } from './libs/models/movie.model';
 
 function App() {
   const [movies, setMovies] = useState<TMovie[] | null>(null);
-
   const [isLoading, setIsLoading] = useState(false);
-  const [moviesLength, setMoviesLength] = useState(0);
+  const [error, setError] = useState<string | null>(null);
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films');
-    const result = await response.json();
-
-    const transformedMovies: TMovie[] = result.results.map((movieData: any) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setMoviesLength(movies ? movies.length : 0);
-    console.log(transformedMovies);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      const result = await response.json();
+      console.log('result parsed', result);
+      const transformedMovies: TMovie[] = result.results.map(
+        (movieData: any) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        }
+      );
+      setMovies(transformedMovies);
+      console.log('transformed', transformedMovies);
+    } catch (error: any) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies?.length) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   // const dummyMovies: TMovie[] = [
   //   {
@@ -49,11 +71,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && moviesLength >= 0 && <MoviesList movies={movies} />}
-        {!isLoading && moviesLength === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </>
   );
 }
